b0066b61fcda57b4d145f5701561a0e8f340d396
eldskald <rafaelbordoni00@gmail.com>
2020-11-22 23:24:40 -0300

    Add hud health node

---
+++ source/ui/health-bar/health-bar.gd
@@ -0,0 +1,36 @@
+extends Control
+
+onready var bar_nodes: HBoxContainer = $Margin/Hbox/Bar
+onready var bar: ProgressBar = $BarSize/ProgressBar
+onready var tween: Tween = $Tween
+onready var total_hp: int = bar_nodes.get_child_count()
+
+
+
+func move_bar(from: int, to: int):
+	from = clamp(from, 0, total_hp)  # Just in case
+	to = clamp(to, 0, total_hp)
+	
+	tween.interpolate_property(bar, "value", to*16, from*16, abs(to - from)*0.5,
+							   Tween.TRANS_LINEAR, Tween.EASE_IN)
+	tween.start()
+
+func activate_tank(tank_number: int):
+	var node = bar_nodes.get_children()[tank_number - 1]
+	node.visible = true
+	node.get_node("AnimationPlayer").play("activate")
+
+func deactivate_tank(tank_number: int):
+	var node = bar_nodes.get_children()[tank_number - 1]
+	node.visible = true
+
+
+
+func initialize(max_hp: int):
+	for i in range(max_hp):
+		bar_nodes.get_children[i].visible = true
+	$BarSize.margin_right = 422 - 16*total_hp
+	self.modulate.a = 1.0
+	move_bar(0, max_hp)
+
+
