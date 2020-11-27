e4a3974646428925ee34f599796dbe6eec95e700
eldskald <rafaelbordoni00@gmail.com>
2020-11-26 23:13:28 -0300

    Add room 7

--- source/actors/enemies/jellyfish/spawner.gd
+++ source/actors/enemies/jellyfish/spawner.gd
@@ -3,6 +3,7 @@ extends Node2D
 export (PackedScene) var jellyfish
 export (String, "Up", "Left", "Down", "Right") var moving = "Right"
 export (float, 0, 500) var moving_speed
+export (float, 0, 2000, 1) var despawn_distance
 
 onready var level = get_tree().get_nodes_in_group("level")[0]
 
@@ -17,7 +18,7 @@ func _ready():
 func _physics_process(_delta):
 	if current_jellyfish != null:
 		if current_jellyfish.is_inside_tree():
-			if (current_jellyfish.position - self.position).length() > 550:
+			if (current_jellyfish.position - self.position).length() > despawn_distance:
 				if current_jellyfish.get_state() == current_jellyfish.READY_STATE:
 					current_jellyfish.queue_free()
 					spawn_jellyfish()
