126ab5afca5ee940cbeabb972fe1212de305bc6d
eldskald <rafaelbordoni00@gmail.com>
2020-11-30 13:10:06 -0300

    Add room 25

--- source/actors/enemies/jellyfish/spawner.gd
+++ source/actors/enemies/jellyfish/spawner.gd
@@ -4,6 +4,8 @@ export (PackedScene) var jellyfish
 export (String, "Up", "Left", "Down", "Right") var moving = "Right"
 export (float, 0, 500) var moving_speed
 export (float, 0, 2000, 1) var despawn_distance
+export (float, 0, 10) var spawn_time
+export (float, 0, 10) var delay_time
 
 onready var level = get_tree().get_nodes_in_group("level")[0]
 
@@ -13,7 +15,14 @@ var current_jellyfish
 
 
 func _ready():
-	spawn_jellyfish()
+	if delay_time <= 0:
+		spawn_jellyfish()
+		if spawn_time > 0:
+			$Timer.start(spawn_time)
+	else:
+		$Delay.start(delay_time)
+
+
 
 func _physics_process(_delta):
 	if current_jellyfish != null:
@@ -21,9 +30,11 @@ func _physics_process(_delta):
 			if (current_jellyfish.position - self.position).length() > despawn_distance:
 				if current_jellyfish.get_state() == current_jellyfish.READY_STATE:
 					current_jellyfish.queue_free()
-					spawn_jellyfish()
+					if spawn_time > 0:
+						spawn_jellyfish()	
 			elif current_jellyfish.get_state() == current_jellyfish.DEAD_STATE:
-				spawn_jellyfish()
+				if spawn_time > 0:
+					spawn_jellyfish()
 
 
 
@@ -34,6 +45,18 @@ func spawn_jellyfish():
 	current_jellyfish.position = self.position
 	level.call_deferred("add_child", current_jellyfish)
 
+
+
+func _on_Timer_timeout():
+	spawn_jellyfish()
+	$Timer.start(spawn_time)
+
+
+
+func _on_Delay_timeout():
+	spawn_jellyfish()
+	if spawn_time > 0:
+		$Timer.start(spawn_time)
 
 
 
